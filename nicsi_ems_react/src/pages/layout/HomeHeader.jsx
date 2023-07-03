dda20d39592c7f3d0e81858c4dc85a40744d24c7
npdevind <nnpdevind@gmail.com>
2023-07-03 23:22:41 +0530

    new client add

--- nicsi_ems_react/src/pages/layout/HomeHeader.jsx
+++ nicsi_ems_react/src/pages/layout/HomeHeader.jsx
@@ -1,7 +1,66 @@
-import { Header, Group, Burger, Container, Button } from "@mantine/core";
+import {
+    createStyles,
+    Menu,
+    Center,
+    Header,
+    Container,
+    Group,
+    Button,
+    Burger,
+    rem,
+    Text,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import { Link } from "react-router-dom";
-import logo from "../../../public/assets/logo.jpg";
+import { IconChevronDown } from "@tabler/icons-react";
+//   import { MantineLogo } from '@mantine/ds';
+
+const HEADER_HEIGHT = rem(60);
+
+const useStyles = createStyles((theme) => ({
+    inner: {
+        height: HEADER_HEIGHT,
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+
+    links: {
+        [theme.fn.smallerThan("sm")]: {
+            display: "none",
+        },
+    },
+
+    burger: {
+        [theme.fn.largerThan("sm")]: {
+            display: "none",
+        },
+    },
+
+    link: {
+        display: "block",
+        lineHeight: 1,
+        padding: `${rem(8)} ${rem(12)}`,
+        borderRadius: theme.radius.sm,
+        textDecoration: "none",
+        color:
+            theme.colorScheme === "dark"
+                ? theme.colors.dark[0]
+                : theme.colors.gray[7],
+        fontSize: theme.fontSizes.sm,
+        fontWeight: 500,
+
+        "&:hover": {
+            backgroundColor:
+                theme.colorScheme === "dark"
+                    ? theme.colors.dark[6]
+                    : theme.colors.gray[0],
+        },
+    },
+
+    linkLabel: {
+        marginRight: rem(5),
+    },
+}));
 
 const HomeHeader = () => {
     const [opened, { toggle }] = useDisclosure(false);
@@ -15,23 +74,73 @@ const HomeHeader = () => {
         { link: "/career", label: "Career" },
     ];
 
-    return (
-        <>
-            <Header className="shadow-lg homeHeader">
-                <Container>
-                    <img src={logo} alt="" className="homeHeaderLogo" />
-                    <Group spacing={5} className="homeGroup">
-                        {links.map((link) => (
-                            <Link key={link.label} to={link.link} className="homeHeaderLink">
-                                <Button variant="gradient" gradient={{ from: "teal", to: "blue", deg: 60 }}>
+    const { classes } = useStyles();
+    // const [opened, { toggle }] = useDisclosure(false);
+    const items = links.map((link) => {
+        const menuItems = link.links?.map((item) => (
+            <Menu.Item key={item.link}>{item.label}</Menu.Item>
+        ));
+
+        if (menuItems) {
+            return (
+                <Menu
+                    key={link.label}
+                    trigger="hover"
+                    transitionProps={{ exitDuration: 0 }}
+                    withinPortal
+                >
+                    <Menu.Target>
+                        <a
+                            href={link.link}
+                            className={classes.link}
+                            onClick={(event) => event.preventDefault()}
+                        >
+                            <Center>
+                                <span className={classes.linkLabel}>
                                     {link.label}
-                                </Button>
-                            </Link>
-                        ))}
-                    </Group>
-                </Container>
-            </Header>
-        </>
+                                </span>
+                                <IconChevronDown size={rem(12)} stroke={1.5} />
+                            </Center>
+                        </a>
+                    </Menu.Target>
+                    <Menu.Dropdown>{menuItems}</Menu.Dropdown>
+                </Menu>
+            );
+        }
+
+        return (
+            <a
+                key={link.label}
+                href={link.link}
+                className={classes.link}
+                onClick={(event) => event.preventDefault()}
+            >
+                {link.label}
+            </a>
+        );
+    });
+
+    return (
+        <Header height={HEADER_HEIGHT} sx={{ borderBottom: 0 }}>
+            <Container className={classes.inner} fluid>
+                <Group>
+                    <Burger
+                        opened={opened}
+                        onClick={toggle}
+                        className={classes.burger}
+                        size="sm"
+                    />
+                    <Text size={23}> NIC EMS</Text>
+                    {/* <MantineLogo size={28} /> */}
+                </Group>
+                <Group spacing={5} className={classes.links}>
+                    {items}
+                </Group>
+                <Button radius="xl" h={30}>
+                    Get early access
+                </Button>
+            </Container>
+        </Header>
     );
 };
 
