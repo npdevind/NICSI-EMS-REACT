f618ded8dc9f56ac137680ebafbaa31e847179f3
npdevind <npdevind@gmail.com>
2023-07-03 17:41:31 +0530

    home header fix

--- nicsi_ems_react/src/pages/layout/HomeHeader.jsx
+++ nicsi_ems_react/src/pages/layout/HomeHeader.jsx
@@ -1,6 +1,7 @@
-import { Header, Group, Burger, Container } from "@mantine/core";
+import { Header, Group, Burger, Container, Button } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { Link } from "react-router-dom";
+import logo from "../../../public/assets/logo.jpg";
 
 const HomeHeader = () => {
     const [opened, { toggle }] = useDisclosure(false);
@@ -11,23 +12,23 @@ const HomeHeader = () => {
             label: "Home",
         },
         { link: "/about", label: "About" },
+        { link: "/career", label: "Career" },
     ];
 
     return (
         <>
-            <Header className="shadow-lg">
+            <Header className="shadow-lg homeHeader">
                 <Container>
-                    <div>
-                        <h5 className="text-2xl font-bold">NICSI-EMS</h5>
-                        <Group spacing={5}>
-                            {links.map((link) => (
-                                <Link key={link.label} to={link.link}>
+                    <img src={logo} alt="" className="homeHeaderLogo" />
+                    <Group spacing={5} className="homeGroup">
+                        {links.map((link) => (
+                            <Link key={link.label} to={link.link} className="homeHeaderLink">
+                                <Button variant="gradient" gradient={{ from: "teal", to: "blue", deg: 60 }}>
                                     {link.label}
-                                </Link>
-                            ))}
-                        </Group>
-                        <Burger opened={opened} onClick={toggle} size="sm" />
-                    </div>
+                                </Button>
+                            </Link>
+                        ))}
+                    </Group>
                 </Container>
             </Header>
         </>
