a46c5c58e249e514c85866365b7c043ee1fdd0bf
npdevind <nnpdevind@gmail.com>
2023-07-06 22:36:05 +0530

    login form set

--- nicsi_ems_client/src/components/auth/Login.jsx
+++ nicsi_ems_client/src/components/auth/Login.jsx
@@ -1,43 +1,114 @@
-import { Card, Input, Checkbox, Button, Typography } from "@material-tailwind/react";
+import {
+    Card,
+    Button,
+    Typography,
+    CardBody,
+    Input,
+} from "@material-tailwind/react";
 import { Link } from "react-router-dom";
-// import HomeHeader from "../../components/layout/HomeHeader";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useState } from "react";
 
 const Login = ({ setType }) => {
     const changedType = () => {
-        console.log("hai");
         setType("register");
     };
+    const [loading, setLoading] = useState(false);
+    const logInSchema = Yup.object().shape({
+        email: Yup.string()
+            .email("Please enter a valid email")
+            .required("This field is required"),
+        password: Yup.string().required("This field is required"),
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: logInSchema,
+        onSubmit: (values, { resetForm }) => {
+            console.log(values);
+            setLoading(true);
+        },
+    });
+
     return (
         <>
-            {/* <div className="flex items-stretch"> */}
-            {/* <HomeHeader /> */}
-            <div className="flex justify-center py-5">
-                <Card color="transparent" shadow={false}>
-                    <Typography variant="h4" color="blue-gray">
-                        Sign In
-                    </Typography>
-                    <Typography color="gray" className="mt-1 font-normal">
-                        Enter your details to log in.
-                    </Typography>
-                    <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
-                        <div className="mb-4 flex flex-col gap-6">
-                            <Input size="lg" label="Email" />
-                            <Input type="password" size="lg" label="Password" />
-                        </div>
-
-                        <Button className="mt-6" fullWidth>
-                            Register
-                        </Button>
-                        <Typography color="gray" className="mt-4 text-center font-normal">
-                            Not have an account?{" "}
-                            <Button className="font-medium text-blue-500 transition-colors hover:text-blue-700 bg-transparent border-none" onClick={() => changedType()}>
-                                Sign Up
-                            </Button>
+            <div className="flex justify-center">
+                <Card className="mt-6 w-96">
+                    <CardBody>
+                        <Typography variant="h5" color="blue-gray">
+                            Sign In
                         </Typography>
-                    </form>
+                        <Typography color="gray" className=" font-normal">
+                            Not have an account?{" "}
+                            <Link
+                                className="font-medium text-blue-500 transition-colors hover:text-blue-700 bg-transparent border-none"
+                                onClick={() => changedType()}
+                            >
+                                Sign Up
+                            </Link>
+                        </Typography>
+
+                        <form
+                            className="mt-8 mb-2 w-90 max-w-screen-xl sm:w-auto"
+                            onSubmit={formik.handleSubmit}
+                        >
+                            <div className="mb-4 flex flex-col gap-6">
+                                <div className="w-[auto]">
+                                    <Input
+                                        name="email"
+                                        size="lg"
+                                        label="Email"
+                                        error={formik.errors.email && true}
+                                        onChange={formik.handleChange}
+                                        value={formik.values.email}
+                                    />
+                                    {formik.errors.email ? (
+                                        <Typography
+                                            variant="small"
+                                            color="red"
+                                            className="font-normal"
+                                        >
+                                            {formik.errors.email}
+                                        </Typography>
+                                    ) : null}
+                                </div>
+
+                                <div className="w-[auto]">
+                                    <Input
+                                        name="password"
+                                        size="lg"
+                                        label="Password"
+                                        type="password"
+                                        error={formik.errors.password && true}
+                                        onChange={formik.handleChange}
+                                        value={formik.values.password}
+                                    />
+                                    {formik.errors.password ? (
+                                        <Typography
+                                            variant="small"
+                                            color="red"
+                                            className="font-normal"
+                                        >
+                                            {formik.errors.password}
+                                        </Typography>
+                                    ) : null}
+                                </div>
+                            </div>
+                            <Button
+                                className="mt-6"
+                                type="submit"
+                                disabled={loading}
+                            >
+                                {loading ? "Loading..." : "Login"}
+                            </Button>
+                        </form>
+                    </CardBody>
                 </Card>
             </div>
-            {/* </div> */}
         </>
     );
 };
